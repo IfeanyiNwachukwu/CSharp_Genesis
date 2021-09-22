9919f6bd0333396c3a9b6e618a12600f5e1b8709
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2021-09-22 15:55:22 +0100

    Added Operator Over Loading

---
+++ ForCSharp/ForCSharp/OperatorOverloading/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForCSharp.OperatorOverloading
+{
+    public class Box
+    {
+        private int length;
+        private int width;
+        private int height;
+
+        public Box(int length, int width, int height)
+        {
+            this.length = length;
+            this.height = height;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public static Box operator +(Box box1, Box box2)
+        {
+            return new Box(box1.height + box2.height, box1.width + box2.width, box1.length + box2.length);
+        }
+
+    }
+}
---
+++ ForCSharp/ForCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ForCSharp.OperatorOverloading;
+using System;
+using System.Diagnostics;
+
+namespace ForCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operator Overloading
+            Box box1 = new(2, 2, 2);
+            Box box2 = new(2, 2, 2);
+
+            Box box3 = box1 + box2;
+
+            Debug.WriteLine($"Length: {box3.GetLength()}");
+            Debug.WriteLine($"Width: {box3.GetWidth()}");
+            Debug.WriteLine($"Length: {box3.GetHeight()}");
+        }
+    }
+}
